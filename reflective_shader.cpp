e07c428c4f2f7438c4fd6dabe55ef90eaf8454b9
Antonio Gonzalez <agonz256@bolt.cs.ucr.edu>
2019-01-25 21:01:17 -0800

    First attempt at reflective_shader

--- reflective_shader.cpp
+++ reflective_shader.cpp
@@ -5,8 +5,14 @@
 vec3 Reflective_Shader::
 Shade_Surface(const Ray& ray,const vec3& intersection_point,
     const vec3& normal,int recursion_depth) const
-{
-    vec3 color;
-    TODO; // determine the color
+{ 
+    vec3 color = {0,0,0};
+    if (recursion_depth > 0) {
+        vec3 r = ray.direction - 2.0 * dot(normal, ray.direction) * normal;
+        Ray reflected_ray(intersection_point, r);
+        color = (1 - reflectivity) * color 
+                + reflectivity * world.Cast_Ray(reflected_ray, recursion_depth-1);
+    } 
+    color = color + shader->Shade_Surface(ray, intersection_point, normal, recursion_depth);
     return color;
 }
